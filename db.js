4be7101c081eda30393cb98fc6bb89ee40843eb6
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-04-04 17:46:58 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/hotels'
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
+require('dotenv').config();
+const mongoURL =process.env.MONGODB_URL;
+
+
+
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
--- server.js
+++ server.js
@@ -33,11 +33,14 @@
 //[3.]Express
 const express = require('express')
 const app = express();
+require('dotenv').config();
 
 const db = require('./db');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT ||3000;
 
 app.get('/', function (req, res) {
   res.send('Welcome to my hotel')
@@ -52,7 +55,9 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000, ()=> {
+
+
+app.listen(PORT, ()=> {
     console.log('listening on port 3000');
 })  
 
